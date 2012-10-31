9745cd3034084da4fdada2574299bcdaffc5bbd1
Mattias Johansson <mattias.johansson@atex.com>
2012-10-31 10:47:29 +0100

    Now querying protocol number and creating socket, no error handling

--- main.c
+++ main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 int main(int argc, char **argv) {
-  printf("Hello world!");
+  // get tcp id and create socket
+  struct protoent *pent = getprotobyname("tcp");
+  socket(PF_INET, SOCK_STREAM, pent->p_proto);
+
+  // bind to :8080
+  // listen to the socket
+  while(1) {
+    // accept
+    printf("Hello world!");
+    // close
+  }
 }
