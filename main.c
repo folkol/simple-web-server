eeef9481d18a5279edd7db7416c52ec5f7ef2161
Mattias Johansson <mattias.johansson@atex.com>
2012-10-31 15:23:00 +0100

    Soem refactoring

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 
@@ -6,48 +7,94 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-int main(int argc, char **argv) {
-  int listen_socket;
-  int client_socket;
-  struct sockaddr_in listen_sock_addr;
-  struct sockaddr current_client;
-  int current_client_len;
+// glooooooobals
+int listen_socket;
+int client_socket;
+struct sockaddr_in listen_sock_addr;
+struct sockaddr current_client;
+int current_client_len;
 
+void init_socket() {
   // set state
   errno = 0;
   listen_sock_addr.sin_family = AF_INET;
-  listen_sock_addr.sin_addr.s_addr = INADDR_ANY;
+  listen_sock_addr.sin_addr.s_addr = htonl(INADDR_ANY);
   listen_sock_addr.sin_port = htons(8080);
 
   // create socket
   if((listen_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
     perror("socket");
-    return -1;
+    exit(-1);
   }
 
   //make it reusable
   int sockopt_on = 1;
   if (setsockopt(listen_socket, SOL_SOCKET, SO_REUSEADDR, &sockopt_on, sizeof(int)) == -1) {
     perror("setsockopt");
-    return 1;
+    exit(-1);
   }
 
   // bind to :8080
   if(bind(listen_socket, (struct sockaddr *) &listen_sock_addr, sizeof(listen_sock_addr)) == -1) {
     perror("bind");
-    return -1;
+    exit(-1);
   }
 
   // listen to the socket
   if(listen(listen_socket, 100) == -1) {
     perror("listen");
-    return -1;
+    exit(-1);
   }
+}
 
+void handle_request() {
   // enter listen loop
-  char* message = "HTTP/1.1 200 OK\n\nHello World!";
+  char send_buffer[1024];
+  char* status_line = "HTTP/1.1 200 OK\x0D\x0A";
+  char* header1 = "Content-Type: text/plain";
+  char* header2 = "Cache-Control: max-age=300";
+  char* body = "Hello World!";
+  int message_length;
+
+  //  "Method SP Request-URI SP HTTP-Version CRLF";
+  // compose response
+  message_length = 0;
+  strcpy(send_buffer, status_line);
+  message_length += strlen(status_line);
+  
+  strcat(send_buffer, header1);
+  message_length += strlen(header1);
+  strcat(send_buffer, "\x0D\x0A");
+  message_length += 2;
+  
+  strcat(send_buffer, header2);
+  message_length += strlen(header2);
+  strcat(send_buffer, "\x0D\x0A");
+  message_length += 2;
+  
+  strcat(send_buffer, "\x0D\x0A");
+  message_length += 2;
+  strcat(send_buffer, body);
+  message_length += strlen(body);
+  
+  if(send(client_socket, send_buffer, message_length, 0) == -1) {
+    perror("send");
+    exit(-1);
+  }
+  
+  fprintf(stderr, "Served!\n");
+  
+  // close
+  if(close(client_socket) == -1) {
+    perror("close");
+    exit(-1);
+  }
+}
+
+int main(int argc, char **argv) {
+  init_socket();
+
   while(1) {
-    // accept
     current_client_len = sizeof(current_client);
     if((client_socket = accept(listen_socket,
                                (struct sockaddr*) &current_client,
@@ -56,56 +103,8 @@ int main(int argc, char **argv) {
       break;
     }
 
-    if(send(client_socket, message, strlen(message), 0) == -1) {
-      perror("send");
-      break;
-    }
-
-    fprintf(stderr, "Served!\n");
-    
-    // close
-    if(close(client_socket) == -1) {
-      perror("close");
-      break;
-    }
+    handle_request();
   }
 
-
-  /*  struct sockaddr_in serv_addr; 
-
-
-struct sockaddr {
-    unsigned short sa_family;   // address family, AF_xxx
-    char           sa_data[14]; // 14 bytes of protocol address
-};
-
-
-
-  char sendBuff[1025];
-  time_t ticks; 
-
-  listenfd = socket(AF_INET, SOCK_STREAM, 0);
-  memset(&serv_addr, '0', sizeof(serv_addr));
-  memset(sendBuff, '0', sizeof(sendBuff)); 
-
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  serv_addr.sin_port = htons(5000); 
-
-  bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
-
-  listen(listenfd, 10); 
-
-  while(1)
-    {
-      connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
-
-      ticks = time(NULL);
-      snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
-      write(connfd, sendBuff, strlen(sendBuff)); 
-
-      close(connfd);
-      sleep(1);
-    }
-  */
+  return 0;
 }
