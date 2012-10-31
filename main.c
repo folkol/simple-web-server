f98a5f100c41ae10d3c9c4684505d40b92799065
Mattias Johansson <mattias.johansson@atex.com>
2012-10-31 11:12:45 +0100

    Simplified socket creation

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-#include <netdb.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
 int main(int argc, char **argv) {
-  // get tcp id and create socket
-  struct protoent *pent = getprotobyname("tcp");
-  socket(PF_INET, SOCK_STREAM, pent->p_proto);
+  // create socket
+  int my_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
   // bind to :8080
+  struct sockaddr my_sock_addr;
+  //  my_sock_addr.
+    //  bind(my_socket, (struct sockaddr *) &my_sock_addr, sizeof(struct sockaddr), 2);
   // listen to the socket
   while(1) {
     // accept
